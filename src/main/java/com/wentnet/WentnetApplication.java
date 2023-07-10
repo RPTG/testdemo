685b273af1c00bda41d905bf6a02070ea3e2c3bb
Tom <1065014620@qq.com>
2023-07-10 14:28:26 +0800

    登录 注册 短信登录

--- src/main/java/com/wentnet/WentnetApplication.java
+++ src/main/java/com/wentnet/WentnetApplication.java
@@ -1,13 +1,16 @@
 package com.wentnet;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class WentnetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WentnetApplication.class, args);
+        log.info("项目启动");
     }
 
 }
